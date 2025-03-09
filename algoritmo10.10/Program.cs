4c01e5fecf3273502785c3da2659207e0f616ebd
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-09 19:24:36 -0300

    Add files via upload

---
+++ algoritmo10.10/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[,] A = new int[4, 4];
+        int somaHachurada = 0;
+
+        
+        Console.WriteLine("Digite os valores para a matriz 4x4:");
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Console.Write($"Elemento [{i + 1},{j + 1}]: ");
+                A[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        // Calcular a soma das partes hachuradas (diagonais)
+        for (int i = 0; i < 4; i++)
+        {
+            // Somando a diagonal principal (A[i,i])
+            somaHachurada += A[i, i];
+
+            // Somando a diagonal secundária (A[i, 3-i])
+            somaHachurada += A[i, 3 - i];
+        }
+
+       
+      
+        somaHachurada -= A[1, 2]; // A célula central é A[1,2] (no índice 1 da linha e 2 da coluna)
+
+        
+        Console.WriteLine($"A soma das partes hachuradas da matriz é: {somaHachurada}");
+    }
+}
---
+++ algoritmo9.9/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        double[] notas = new double[6];
+
+     
+        Console.WriteLine("Digite as 6 notas dos juízes (de 0 a 10):");
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write($"Nota do juiz {i + 1}: ");
+            notas[i] = Convert.ToDouble(Console.ReadLine());
+        }
+
+       
+        double maiorNota = notas[0];
+        double menorNota = notas[0];
+        for (int i = 1; i < 6; i++)
+        {
+            if (notas[i] > maiorNota)
+                maiorNota = notas[i];
+
+            if (notas[i] < menorNota)
+                menorNota = notas[i];
+        }
+
+        // Calcular a soma das notas excluindo a maior e a menor
+        double somaFinal = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            if (notas[i] != maiorNota && notas[i] != menorNota)
+            {
+                somaFinal += notas[i];
+            }
+        }
+
+        
+        Console.WriteLine($"A nota final do atleta, excluindo a maior e a menor nota, é: {somaFinal:F2}");
+    }
+}
