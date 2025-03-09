5c4ea13a50de78a80d617931c549814cb98e4a70
rodrigo792 <rodrigoalvarenga790@gmail.com>
2025-03-09 19:21:45 -0300

    Add files via upload

---
+++ algoritmo1.1/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Informe o preço atual do produto (R$): ");
+        double precoAtual = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe a venda média mensal do produto: ");
+        int vendaMediaMensal = Convert.ToInt32(Console.ReadLine());
+
+       
+        double novoPreco = precoAtual;
+
+        if (vendaMediaMensal < 500)
+        {
+            if (precoAtual < 30.00)
+            {
+                
+                novoPreco = precoAtual * 1.10;
+                Console.WriteLine($"O preço do produto será aumentado em 10%. Novo preço: R${novoPreco:F2}");
+            }
+            else
+            {
+                
+                novoPreco = precoAtual * 0.80;
+                Console.WriteLine($"O preço do produto será reduzido em 20%. Novo preço: R${novoPreco:F2}");
+            }
+        }
+        else
+        {
+            if (precoAtual >= 30.00)
+            {
+               
+                Console.WriteLine("O preço do produto não sofrerá reajuste.");
+            }
+        }
+    }
+}
---
+++ algoritmo2.2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.Write("Informe a altura da pessoa (em metros): ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Informe o sexo da pessoa (M para masculino ou F para feminino): ");
+        char sexo = Char.ToUpper(Convert.ToChar(Console.ReadLine()));
+
+        double pesoIdeal = 0;
+
+        // Calcular o peso ideal com base no sexo
+        if (sexo == 'M')
+        {
+            // Fórmula para homens
+            pesoIdeal = (72.7 * altura) - 58;
+        }
+        else if (sexo == 'F')
+        {
+            // Fórmula para mulheres
+            pesoIdeal = (62.1 * altura) - 44.7;
+        }
+        else
+        {
+            Console.WriteLine("Sexo inválido! Informe 'M' para masculino ou 'F' para feminino.");
+            return;
+        }
+
+        
+        Console.WriteLine($"O peso ideal para uma pessoa de altura {altura:F2}m e sexo {sexo} é: {pesoIdeal:F2} kg");
+    }
+}
